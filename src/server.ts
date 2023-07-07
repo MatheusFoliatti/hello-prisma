c6363d1ab55787b1ca4b2ff76fdc054b91b0d556
MatheusFoliatti <matheusfoliatti@gmail.com>
2023-07-07 20:59:15 -0300

    git bruno

--- src/server.ts
+++ src/server.ts
@@ -2,44 +2,91 @@ import express from 'express'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+
 const server = express()
 const port = 3333
 
 server.use(express.json())
 
-server.get("/", async (req,res) => {
+server.use( (req, res, next) => {
+    console.log(
+        `[${new Date().getTime()}] - ${req.headers["x-forwarded-for"] || req.socket.remoteAddress} - ${req.method} - ${req.originalUrl}`
+    )
+    next()
+})
+
+server.get('/', async (req,res) => {
     const getAll = await prisma.user.findMany()
 
     res.json(getAll)
+
 })
 
 interface IRequest {
-    name: string;
-    email: string;
+    name: string
+    email: string
 }
 
-server.post('/', async (req,res) =>{
-    const {name, email}:IRequest = req.body
+server.post('/', async (req,res) => {
+   const { name , email}:IRequest = req.body
 
-    const userExist = await prisma.user.findFirst({
-        where:{
+   const userExist = await prisma.user.findFirst({
+    where:{
+        email
+    }
+   })
+   
+   if(userExist) return res.status(404).json({error: true, message: "Usuário já existe"})
+
+   const createUser = await prisma.user.create({
+        data: {
+            name,
             email
         }
     })
+   
+    res.json(createUser)
 
-    if(userExist) return res.status(404).json({error: true, message: "Usuário já existe" })
+})
 
-    const createUser = await prisma.user.create({
-        data:{
+server.put('/:id', async (req,res) => {
+   const { name , email}:IRequest = req.body
+   const { id } = req.params
+
+    const userExist = await prisma.user.findFirst({
+    where:{
+        id
+    }
+   })
+   
+   if(!userExist) return res.status(400).json({error: true, message: "Usuário não existe"})
+
+   const updateUser = await prisma.user.update({
+        where:{
+            id
+        },
+        data: {
             name,
-            email,
+            email
+        },
+        select: {
+            name: true,
+            email: true,
+            post: true
         }
     })
+   
+    res.json(updateUser)
 
-    res.json(createUser)
-    
+})
+
+
+
+server.use((req, res, next) => {
+    res.status(404).json({message: "Erro ao acessar a rota!"})
 })
 
 server.listen(port, () => {
-    console.log(`O servidor está na esteira... - http://localhost:${port}`)
+    console.log(`Server in running - http://localhost:${port} `);
+    
 })
